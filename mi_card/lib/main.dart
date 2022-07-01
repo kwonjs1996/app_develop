92639eeba459812284dea45fc18acd42fd13c4a3
kwonjs1996 <kwonjs1996@gmail.com>
2022-07-01 11:38:51 +0900

    fontFamily 패키지 추가

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -9,8 +9,36 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
-        body: Container(),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundColor: Colors.red,
+                backgroundImage: AssetImage('assets/damzang.jpeg'),
+              ),
+              Text(
+                'Dam Zang',
+                style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 40,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'LORD OF HOUSE',
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 20,
+                  color: Colors.teal.shade100,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
