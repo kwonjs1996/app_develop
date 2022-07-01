ae8ec40c30cc2b2c108b3e6906b5fcd7d59fca22
kwonjs1996 <kwonjs1996@gmail.com>
2022-07-01 17:17:02 +0900

    Card(leading, title) 위젯 사용하여 명함 앱 만들기

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -11,32 +11,71 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(
-            children: [
-              CircleAvatar(
-                radius: 50.0,
-                backgroundColor: Colors.red,
-                backgroundImage: AssetImage('assets/damzang.jpeg'),
-              ),
-              Text(
-                'Dam Zang',
-                style: TextStyle(
-                    fontFamily: 'Pacifico',
-                    fontSize: 40,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold),
-              ),
-              Text(
-                'LORD OF HOUSE',
-                style: TextStyle(
-                  fontFamily: 'SourceSansPro',
-                  fontSize: 20,
-                  color: Colors.teal.shade100,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 2.5,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundColor: Colors.red,
+                  backgroundImage: AssetImage('assets/damzang.jpeg'),
                 ),
-              )
-            ],
+                Text(
+                  'Dam Zang',
+                  style: TextStyle(
+                      fontFamily: 'Pacifico',
+                      fontSize: 40,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'LORD OF HOUSE',
+                  style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    fontSize: 20,
+                    color: Colors.teal.shade100,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.5,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 300.0,
+                  child: Divider(
+                    color: Colors.teal[100],
+                  ),
+                ),
+                Card(
+                    margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.phone,
+                        size: 20.0,
+                        color: Colors.teal[900],
+                      ),
+                      title: Text(
+                        '+82 10 123 1234',
+                        style: TextStyle(
+                            fontFamily: 'SourceSansPro', fontSize: 15.0),
+                      ),
+                    )),
+                Card(
+                    margin: EdgeInsets.fromLTRB(20, 0, 20, 20),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.email,
+                        color: Colors.teal[900],
+                      ),
+                      title: Text(
+                        'asdf123@gmail.com',
+                        style: TextStyle(
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 15.0,
+                        ),
+                      ),
+                    ))
+              ],
+            ),
           ),
         ),
       ),
