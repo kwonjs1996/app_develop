88dae2173dc6dbcb8b18275c1060866846a6258f
kwonjs1996 <kwonjs1996@gmail.com>
2022-07-05 20:53:30 +0900

    flutter 함수 만들기

--- dicee_flutter/lib/main.dart
+++ dicee_flutter/lib/main.dart
@@ -24,28 +24,34 @@ class DicePage extends StatefulWidget {
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 1;
+
+  void randomDiceImage() {
+    leftDiceNumber = Random().nextInt(6) + 1;
+    rightDiceNumber = Random().nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
         children: [
           Expanded(
-            child: FlatButton(
+            child: TextButton(
               onPressed: () {
                 // setState =>  Build 메소드 안의 내용중 어떤 부분이 변경되었는지 확인하라!
                 // 바뀐 변수로 업데이트 한다.
                 setState(() {
-                  leftDiceNumber = Random().nextInt(6) + 1;
+                  randomDiceImage();
                 });
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
           Expanded(
-            child: FlatButton(
+            child: TextButton(
               onPressed: () {
                 setState(() {
-                  rightDiceNumber = Random().nextInt(6) + 1;
+                  randomDiceImage();
                 });
               },
               child: Image.asset('images/dice$rightDiceNumber.png'),
