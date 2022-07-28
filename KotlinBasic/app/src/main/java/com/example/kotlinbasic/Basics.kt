8d238ec240f311c216704fc41b1765efbac00455
kwonjs1996 <kwonjs1996@gmail.com>
2022-07-28 22:18:00 +0900

    상속

--- KotlinBasic/app/src/main/java/com/example/kotlinbasic/Basics.kt
+++ KotlinBasic/app/src/main/java/com/example/kotlinbasic/Basics.kt
@@ -1,18 +1,50 @@
 package com.example.kotlinbasic
 
+interface Drivable{
+    val maxSpeed: Double
+    fun drive(): String
+    fun brake(){
+        println("The drivable is braking")
+    }
+}
+
+
+// Super Class, Parent Class, Base Class of ElectricCar
+open class Car(val name: String, val brand: String){
+    open var range: Double = 0.0
+    fun extendRange(amount: Double){
+        if(amount > 0){
+            range += amount
+        }
+    }
+    open fun drive(distance: Double){
+        println("Drove for $distance KM")
+    }
+}
+// Sub Class, Child Class, Derived Class of Car
+// Parent Class의 매개변수가 "모두" 입력되어야 함, 추가적인 매개변수 또한 입련 가능
+class ElectricCar(name: String,brand: String, batteryLife: Double)
+    : Car(name, brand){
+
+    var chargerType = "Type1"
+
+    override var range = batteryLife/6
+
+    override fun drive(distance: Double) {
+        println("Drove for $distance KM on electricity")
+    }
+    fun drive(){
+        println("Drove for $range KM on Electricity")
+    }
+}
 
 fun main(){
-   val phone = MobilePhone("Android", "Samsung","S20")
-   println(phone.battery)
-   phone.chargeBattery(50)
-   println(phone.battery)
-}
-class MobilePhone(osName: String, brand: String, model: String){
-   var battery = 0
-   init {
-      println("The phone $model from $brand uses $osName as its Operating System")
-   }
-   fun chargeBattery(chargedBy : Int){
-      battery += chargedBy
-   }
+    var audiA3 = Car("A3", "Audi")
+    var teslaS = ElectricCar("S-Model", "Tesla", 85.0)
+
+    teslaS.extendRange(200.0)
+    audiA3.drive(200.0)
+    teslaS.drive(200.0)
+    println(teslaS.range)
+
 }
