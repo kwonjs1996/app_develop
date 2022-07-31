dcac04429dbf58da3022138536f0c52085ba5d36
kwonjs1996 <kwonjs1996@gmail.com>
2022-07-31 20:13:20 +0900

    AgeInMinutes app

--- MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -6,35 +6,77 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    // activity_main 에서 바꿔서 오류가 나는걸 방지하기 위해서 private 으로 생성한다.
+    private var tvSelectedDate : TextView? = null
+    private var tvAgeInMinutes : TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val btnDatePicker: Button = findViewById(R.id.btnDatePicker)
+        tvSelectedDate = findViewById(R.id.tvSelectedDate)
+        tvAgeInMinutes = findViewById(R.id.tvAgeInMinutes)
+
         btnDatePicker.setOnClickListener {
             clickDataPicker()
         }
 
     }
 
-    fun clickDataPicker() {
+    private fun clickDataPicker() {
         val myCalender = Calendar.getInstance()
         val year = myCalender.get(Calendar.YEAR)
         val month = myCalender.get(Calendar.MONTH)
         val day = myCalender.get(Calendar.DAY_OF_MONTH)
         // 버튼 클릭시 캘린더 구현, ok 눌렀을 때 토스트 메시지 구현, cancel에는 토스트메시지 구현되지 않음.
-        DatePickerDialog(
+        val dpd = DatePickerDialog(
             this,
-            DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
-                Toast.makeText(this, "preesed", Toast.LENGTH_LONG).show()
+            { view, selectedYear, selectedMonth, selectedDayOfMonth ->
+                Toast.makeText(this,
+                    "Year was $selectedYear, Month was ${selectedMonth + 1}, day of month was $selectedDayOfMonth",
+                    Toast.LENGTH_LONG).show()
+
+                val selectedDate = "$selectedDayOfMonth/${selectedMonth + 1}/$selectedYear"
+
+                tvSelectedDate?.text = selectedDate
+
+                // 계산하기위해 선택한 날짜를 데이터로 변수 생성하기
+                val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+
+                val theDate = sdf.parse(selectedDate)
+
+                // 살아온 날을 분으로 계산하기
+                theDate?.let {
+                    val selectedDateInMinutes = theDate.time / 60000
+
+                    val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+                    currentDate?.let {
+                        val currentDateInMinutes = currentDate.time/ 60000
+
+                        val differenceInMinutes = currentDateInMinutes - selectedDateInMinutes
+
+                        tvAgeInMinutes?.text = differenceInMinutes.toString()
+                    }
+
+
+                }
+
+
+
+
+
             },
             year,
             month,
             day
-        ).show()
+        )
+        dpd.datePicker.maxDate = System.currentTimeMillis() - 86400000
+        dpd.show()
 //         DatePickerDialog(this, {_,_,_,_ ->
 //             Toast.makeText(this, "preesed", Toast.LENGTH_LONG).show()
 //         },year,month,day).show()
