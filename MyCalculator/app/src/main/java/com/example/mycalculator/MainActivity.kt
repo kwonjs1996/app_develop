895e9240a61567fc425a15fb9111b1e5302616c4
kwonjs1996 <kwonjs1996@gmail.com>
2022-08-02 11:01:48 +0900

    fun - + * / , 음수 연산

--- MyCalculator/app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ MyCalculator/app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -6,12 +6,14 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import java.lang.ArithmeticException
 
 class MainActivity : AppCompatActivity() {
-    private var tvInput : TextView? = null
+    private var tvInput: TextView? = null
+
     // tvInput 이라는 변수를 널러블로 선언한다 그리고 안에 tvInput 을 Id 로 찾아서 객체로 만든다.
-    var lastNumeric : Boolean = false
-    var lastDot : Boolean = false
+    var lastNumeric: Boolean = false
+    var lastDot: Boolean = false
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +22,7 @@ class MainActivity : AppCompatActivity() {
         tvInput = findViewById(R.id.tvInput)
     }
 
-    fun onDigit(view : View){
+    fun onDigit(view: View) {
         // view == 각각 불러오는 button 과 같다.
         // null 을 체크함으로 인해서 일일히 if 문으로 확인하지 않더라도 값이 null 이면 코드를 실행시키지 않는다.
         tvInput?.append((view as Button).text)
@@ -30,16 +32,110 @@ class MainActivity : AppCompatActivity() {
         lastDot = false
         // "." 이 안눌리는 상황이기 때문에 lastNumeric, lastDot 변수 값을 지정한다. onDecimalPoint() if 문이 계속 돌아갈 수 있게
     }
-    fun onClear(view: View){
+
+    fun onClear(view: View) {
         tvInput?.text = ""
     }
 
-    fun onDecimalPoint(view: View){
-        if(lastNumeric && !lastDot){
+    fun onDecimalPoint(view: View) {
+        if (lastNumeric && !lastDot) {
             tvInput?.append(".")
             // flag(플래그) 라고 한다고 함.
             lastNumeric = false
             lastDot = true
+        }
+    }
+
+    fun onOperator(view: View) {
+        // tvInput 을 널러블로 지정 할 수 없어서 사용.
+        // tvInput 이 null 이 아니고 text 가 null 이 아닐때 그 값을 it 으로 한다.
+        tvInput?.text?.let {
+            // 연산자가 선택 후 다른 연산자를 선택 할 수 없게 하기 위해서 넣는 장치
+            if (lastNumeric && !isOperatorAdded(it.toString())) {
+                tvInput?.append((view as Button).text)
+                lastNumeric = false
+                lastDot = false
+            }
+        }
+    }
+
+    fun onEqual(view: View) {
+        if (lastNumeric) {
+            var tvValue = tvInput?.text.toString()
+            var prefix = ""
+
+            try {
+                if (tvValue.startsWith("-")) {
+                    prefix = tvValue.substring(1)
+                }
+                if (tvValue.contains("-")){
+                    val splitValue = tvValue.split("-")
+
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() - two.toDouble()).toString())
+                }else if (tvValue.contains("+")){
+                    val splitValue = tvValue.split("+")
+
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() + two.toDouble()).toString())
+                }else if (tvValue.contains("*")){
+                    val splitValue = tvValue.split("*")
+
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() * two.toDouble()).toString())
+                }else if (tvValue.contains("/")){
+                    val splitValue = tvValue.split("/")
+
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() / two.toDouble()).toString())
+                }
+
+            } catch (e: ArithmeticException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun removeZeroAfterDot(result: String): String {
+        var value = result
+        if (result.contains(".0"))
+            value = result.substring(0, result.length - 2)
+
+        return value
+    }
+
+    private fun isOperatorAdded(value: String): Boolean {
+        return if (value.startsWith("-")) {
+            false
+        } else {
+            value.contains("/")
+                    || value.contains("*")
+                    || value.contains("+")
+                    || value.contains("-")
         }
     }
 }
