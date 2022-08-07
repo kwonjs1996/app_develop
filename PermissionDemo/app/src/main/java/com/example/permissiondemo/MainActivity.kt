c63fc595c2b121ff3ffca2ceb95b677e65563751
kwonjs1996 <kwonjs1996@gmail.com>
2022-08-07 21:27:33 +0900

    받아야 할 Permission 이 두개 이상일때

--- PermissionDemo/app/src/main/java/com/example/permissiondemo/MainActivity.kt
+++ PermissionDemo/app/src/main/java/com/example/permissiondemo/MainActivity.kt
@@ -12,39 +12,96 @@ import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
 
-    private val cameraResultLauncher : ActivityResultLauncher<String> =
+    private val cameraResultLauncher: ActivityResultLauncher<String> =
         // register 로 결과를 불러와서, .RequestPermission 메소드에 입력하고 람다식으로 승인됐는지 확인한다.
         registerForActivityResult(
-            ActivityResultContracts.RequestPermission()){
-                isGranted ->
-                if (isGranted){
-                    Toast.makeText(this, "Permission granted for camera", Toast.LENGTH_SHORT).show()
-                }else{
-                    Toast.makeText(this, "Permission denied for camera", Toast.LENGTH_SHORT).show()
-                }
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted ->
+            if (isGranted) {
+                Toast.makeText(this, "Permission granted for camera", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Permission denied for camera", Toast.LENGTH_SHORT).show()
             }
+        }
+
+    private val cameraAndLocationResultLauncher: ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            permissions.entries.forEach {
+                // 승인 요청한 권한, ex) 카메라, 위치 등
+                val permissionName = it.key
+                // 권한을 가지고 있는지에 대한  == 권한 승인여부
+                val isGranted = it.value
+                // Permission 에 대한 권한 체크 구문.
+                if (isGranted) {
+                    if (permissionName == Manifest.permission.ACCESS_FINE_LOCATION) {
+                        Toast.makeText(this, "Permission granted for location", Toast.LENGTH_SHORT)
+                            .show()
+                    } else if (permissionName == Manifest.permission.ACCESS_COARSE_LOCATION) {
+                        Toast.makeText(
+                            this,
+                            "Permission granted for COARSE_LOCATION",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                        Toast.makeText(this, "Permission granted for Camera", Toast.LENGTH_SHORT)
+                            .show()
+
+                    }
+                } else {
+                    if (permissionName == Manifest.permission.ACCESS_FINE_LOCATION) {
+                        Toast.makeText(
+                            this,
+                            "Permission denied for fine location",
+                            Toast.LENGTH_SHORT
+                        )
+                            .show()
+                    } else if (permissionName == Manifest.permission.ACCESS_COARSE_LOCATION) {
+                        Toast.makeText(
+                            this,
+                            "Permission denied for COARSE_LOCATION",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                        Toast.makeText(this, "Permission denied for Camera", Toast.LENGTH_SHORT)
+                            .show()
+
+                    }
+                }
+
+            }
+        }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val btnPermissionCamera : Button = findViewById(R.id.btn_permission_camera)
-        btnPermissionCamera.setOnClickListener{
+        val btnPermissionCamera: Button = findViewById(R.id.btn_permission_camera)
+        btnPermissionCamera.setOnClickListener {
             // if 안드로이드 버전이 몇인지, 카메라에 대한 permission 이 수락 되었는지 확인
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
-                    shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)){
+                shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)
+            ) {
                 showRationaleDialog("Camera Permission", "Can not be used, denied permission")
-            }else{
-                cameraResultLauncher.launch(Manifest.permission.CAMERA)
+            } else {
+                // Permission 이 두개 이상일 때
+                cameraAndLocationResultLauncher.launch(
+                    arrayOf(
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    )
+                )
             }
         }
     }
 
-    private fun showRationaleDialog (title: String, message : String){
-        val builder : AlertDialog.Builder = AlertDialog.Builder(this)
+    private fun showRationaleDialog(title: String, message: String) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
         builder.setTitle(title)
             .setMessage(message)
-            .setPositiveButton("Cancel"){dialog, _ ->
+            .setPositiveButton("Cancel") { dialog, _ ->
                 dialog.dismiss()
             }
         builder.create().show()
