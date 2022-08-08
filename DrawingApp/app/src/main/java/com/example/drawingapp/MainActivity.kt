8440ebce386561d1b590bac9d8f8c52220320d97
kwonjs1996 <kwonjs1996@gmail.com>
2022-08-08 19:52:42 +0900

    storage permission

--- DrawingApp/app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ DrawingApp/app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.drawingapp
 
+import android.Manifest
 import android.app.Dialog
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 
@@ -14,6 +21,24 @@ class MainActivity : AppCompatActivity() {
 
     private var drawingView: DrawingView? = null
     private var mImageButtonCurrentPaint: ImageButton? = null
+    private val storageResultLauncher : ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ){permissions ->
+            permissions.entries.forEach{
+                val permissionName = it.key
+                val isGranted = it.value
+                if(isGranted){
+                    if (permissionName == Manifest.permission.READ_EXTERNAL_STORAGE){
+                        Toast.makeText(this, "Permission granted for Storage", Toast.LENGTH_SHORT).show()
+                    }
+                }else{
+                    if (permissionName != Manifest.permission.READ_EXTERNAL_STORAGE){
+                        Toast.makeText(this, "Permission denied for Storage", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,6 +60,10 @@ class MainActivity : AppCompatActivity() {
         val ib_brush : ImageButton = findViewById(R.id.ib_brush)
         ib_brush.setOnClickListener {
             showBrushSizeChooserDialog()
+        }
+        val btnStoragePermission : ImageButton = findViewById(R.id.ib_storage)
+        btnStoragePermission.setOnClickListener{
+            requestStoragePermission()
         }
     }
 
@@ -81,5 +110,28 @@ class MainActivity : AppCompatActivity() {
             mImageButtonCurrentPaint = view
 
         }
+    }
+
+    private fun requestStoragePermission(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+            ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_EXTERNAL_STORAGE)){
+            showRationaleDialog("Storage Permission", "Can not be used. denied permission")
+        }else{
+            storageResultLauncher.launch(
+                arrayOf(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                )
+            )
+        }
+    }
+
+    private fun showRationaleDialog (title: String, message : String){
+        val builder : AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("Cancel"){ dialog, _ ->
+                dialog.dismiss()
+            }
+        builder.create().show()
     }
 }
