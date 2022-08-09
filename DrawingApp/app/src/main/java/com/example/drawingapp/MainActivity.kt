6aaedd9f8e3752e6e2a93c751bb2f6be1dc744ae
kwonjs1996 <kwonjs1996@gmail.com>
2022-08-09 20:19:20 +0900

    save 버튼 추가, 기능구현 Coroutine

--- DrawingApp/app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ DrawingApp/app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.drawingapp
 import android.Manifest
 import android.app.Dialog
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -15,40 +19,50 @@ import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
     private var drawingView: DrawingView? = null
     private var mImageButtonCurrentPaint: ImageButton? = null
-    val openGalleryLauncher : ActivityResultLauncher<Intent> =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-            result ->
-            if(result.resultCode == RESULT_OK && result.data != null){
-                val imageBackGround : ImageView = findViewById(R.id.iv_background)
+    val openGalleryLauncher: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK && result.data != null) {
+                val imageBackGround: ImageView = findViewById(R.id.iv_background)
                 // 데이터 자체를 받아오는 것이 아니라 데이터의 위치를 이용하여 나타낸다.
                 imageBackGround.setImageURI(result.data?.data)
             }
         }
 
-    private val storageResultLauncher : ActivityResultLauncher<Array<String>> =
+    private val storageResultLauncher: ActivityResultLauncher<Array<String>> =
         registerForActivityResult(
             ActivityResultContracts.RequestMultiplePermissions()
-        ){permissions ->
-            permissions.entries.forEach{
+        ) { permissions ->
+            permissions.entries.forEach {
                 val permissionName = it.key
                 val isGranted = it.value
-                if(isGranted){
-                    if (permissionName == Manifest.permission.READ_EXTERNAL_STORAGE){
-                        Toast.makeText(this, "Permission granted for Storage", Toast.LENGTH_SHORT).show()
+                if (isGranted) {
+                    if (permissionName == Manifest.permission.READ_EXTERNAL_STORAGE) {
+                        Toast.makeText(this, "Permission granted for Storage", Toast.LENGTH_SHORT)
+                            .show()
                         // intent 를 이용하여 다른 앱으로 넘어갈 수 있다.
                         // URI 는 기기내의 위치를 뜻함.
-                        val pickIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.
-                        Media.EXTERNAL_CONTENT_URI)
+                        val pickIntent = Intent(
+                            Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                        )
                         openGalleryLauncher.launch(pickIntent)
                     }
-                }else{
-                    if (permissionName != Manifest.permission.READ_EXTERNAL_STORAGE){
-                        Toast.makeText(this, "Permission denied for Storage", Toast.LENGTH_SHORT).show()
+                } else {
+                    if (permissionName != Manifest.permission.READ_EXTERNAL_STORAGE) {
+                        Toast.makeText(this, "Permission denied for Storage", Toast.LENGTH_SHORT)
+                            .show()
                     }
                 }
             }
@@ -71,41 +85,52 @@ class MainActivity : AppCompatActivity() {
             ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
         )
 
-        val ibBrush : ImageButton = findViewById(R.id.ib_brush)
+        val ibBrush: ImageButton = findViewById(R.id.ib_brush)
         ibBrush.setOnClickListener {
             showBrushSizeChooserDialog()
         }
-        val ibStorage : ImageButton = findViewById(R.id.ib_storage)
-        ibStorage.setOnClickListener{
+        val ibStorage: ImageButton = findViewById(R.id.ib_storage)
+        ibStorage.setOnClickListener {
             requestStoragePermission()
         }
-        val ibUndo : ImageButton = findViewById(R.id.ib_undo)
+        val ibUndo: ImageButton = findViewById(R.id.ib_undo)
         ibUndo.setOnClickListener {
             drawingView?.onClickUndo()
         }
-        val ibRedo : ImageButton = findViewById(R.id.ib_redo)
+        val ibRedo: ImageButton = findViewById(R.id.ib_redo)
         ibRedo.setOnClickListener {
             drawingView?.onClickRedo()
         }
 
+        val ibSave: ImageButton = findViewById(R.id.ib_save)
+        ibSave.setOnClickListener {
+            if (isReadStorageAllowed()){
+                lifecycleScope.launch{
+                    // 모든 그려진 view (background, draw)를 가지고 있는 FrameLayout 을 가져온다.
+                    val flDrawingView : FrameLayout = findViewById(R.id.fl_drawing_view_container)
+                    // getBitmapFromView 에서 bitmap 을 리턴하고 그 bitmap 을 saveBitmapFile 로 전달.
+                    saveBitmapFile(getBitmapFromView(flDrawingView))
+                }
+            }
+        }
     }
 
-    private fun showBrushSizeChooserDialog(){
+    private fun showBrushSizeChooserDialog() {
         val brushDialog = Dialog(this)
         brushDialog.setContentView(R.layout.dialog_brush_size)
         brushDialog.setTitle("Brush size: ")
-        val smallBtn : ImageButton = brushDialog.findViewById(R.id.ib_small_brush)
-        smallBtn.setOnClickListener{
+        val smallBtn: ImageButton = brushDialog.findViewById(R.id.ib_small_brush)
+        smallBtn.setOnClickListener {
             drawingView?.setSizeForBrush(10.toFloat())
             brushDialog.dismiss()
         }
-        val mediumBtn : ImageButton = brushDialog.findViewById(R.id.ib_medium_brush)
-        mediumBtn.setOnClickListener{
+        val mediumBtn: ImageButton = brushDialog.findViewById(R.id.ib_medium_brush)
+        mediumBtn.setOnClickListener {
             drawingView?.setSizeForBrush(20.toFloat())
             brushDialog.dismiss()
         }
-        val largeBtn : ImageButton = brushDialog.findViewById(R.id.ib_large_brush)
-        largeBtn.setOnClickListener{
+        val largeBtn: ImageButton = brushDialog.findViewById(R.id.ib_large_brush)
+        largeBtn.setOnClickListener {
             drawingView?.setSizeForBrush(30.toFloat())
             brushDialog.dismiss()
         }
@@ -113,8 +138,8 @@ class MainActivity : AppCompatActivity() {
         brushDialog.show()
     }
 
-    fun paintClicked(view: View){
-        if (view !== mImageButtonCurrentPaint){
+    fun paintClicked(view: View) {
+        if (view !== mImageButtonCurrentPaint) {
             val imageButton = view as ImageButton
             val colorTag = imageButton.tag.toString()
             drawingView?.setColor(colorTag)
@@ -135,26 +160,99 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun requestStoragePermission(){
+    private fun isReadStorageAllowed() : Boolean{
+        // checkSelfPermission 의 값이 int 로 가지고 있다면 0 없다면 1 로 들어온다.
+        val result = ContextCompat.checkSelfPermission(this,
+        Manifest.permission.READ_EXTERNAL_STORAGE)
+        // PackageManager.PERMISSION_GRANTED 의 값
+        // 또한 0 or 1 로 들어오기 때문에 Boolean 타입으로 리턴 할 수 있다.
+        return result == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestStoragePermission() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
-            ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_EXTERNAL_STORAGE)){
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+        ) {
             showRationaleDialog("Storage Permission", "Can not be used. denied permission")
-        }else{
+        } else {
             storageResultLauncher.launch(
                 arrayOf(
                     Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
                 )
             )
         }
     }
 
-    private fun showRationaleDialog (title: String, message : String){
-        val builder : AlertDialog.Builder = AlertDialog.Builder(this)
+    private fun showRationaleDialog(title: String, message: String) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
         builder.setTitle(title)
             .setMessage(message)
-            .setPositiveButton("Cancel"){ dialog, _ ->
+            .setPositiveButton("Cancel") { dialog, _ ->
                 dialog.dismiss()
             }
         builder.create().show()
     }
+
+    // Bitmap 이미지로 그린 그림 저장하기, 전제조건 View 안에 그림을 Array 로 저장하고 있기 때문에
+    // View 를 비트맵 이미지로 가져오는 식으로 fun 을 만든다.
+    private fun getBitmapFromView(view: View): Bitmap {
+        val returnedBitmap = Bitmap.createBitmap(
+            view.width, view.height, Bitmap.Config.ARGB_8888
+        )
+        val canvas = Canvas(returnedBitmap)
+        val bgDrawable = view.background
+        if (bgDrawable != null) {
+            bgDrawable.draw(canvas)
+        } else {
+            canvas.drawColor(Color.WHITE)
+        }
+        view.draw(canvas)
+
+        return returnedBitmap
+    }
+
+    // Coroutine 사용하기
+    private suspend fun saveBitmapFile(mBitmap: Bitmap?): String {
+        var result = ""
+        withContext(Dispatchers.IO) {
+            if (mBitmap != null) {
+                try {
+                    val bytes = ByteArrayOutputStream()
+                    mBitmap.compress(Bitmap.CompressFormat.PNG, 90, bytes)
+
+                    val f = File(
+                        externalCacheDir?.absoluteFile.toString()
+                                + File.separator + "DrawingApp_" + System.currentTimeMillis() / 1000 + ".png"
+                    )
+                    val fo = FileOutputStream(f)
+                    fo.write(bytes.toByteArray())
+                    fo.close()
+                    result = f.absolutePath
+
+                    runOnUiThread {
+                        if (result.isNotEmpty()) {
+                            Toast.makeText(
+                                this@MainActivity, "File saved successfully :$result",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        } else {
+                            Toast.makeText(
+                                this@MainActivity, "SomeThing went wrong while saving the file",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    }
+                } catch (e: Exception) {
+                    result = ""
+                    e.printStackTrace()
+                }
+            }
+        }
+        return result
+    }
+
 }
