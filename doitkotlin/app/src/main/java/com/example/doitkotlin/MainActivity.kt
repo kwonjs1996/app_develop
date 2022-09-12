94320dee7338c534f98177146cead14d694cfc71
kwonjs1996 <kwonjs1996@gmail.com>
2022-09-12 16:30:04 +0900

    ViewBinding

--- doitkotlin/app/src/main/java/com/example/doitkotlin/MainActivity.kt
+++ doitkotlin/app/src/main/java/com/example/doitkotlin/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.doitkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.doitkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 바인딩 객체 획득
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        // 액티비티 화면 출력
+        setContentView(binding.root)
+
+        // 뷰 객체 이용
+        binding.visibleBtn.setOnClickListener{
+            binding.targetView.visibility = View.VISIBLE
+        }
+        binding.invisibleBtn.setOnClickListener {
+            binding.targetView.visibility = View.INVISIBLE
+        }
     }
 }
