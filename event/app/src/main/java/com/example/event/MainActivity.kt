e4139c30d3be2db2e253d1d523a3ace3cd5a2c03
kwonjs1996 <kwonjs1996@gmail.com>
2022-10-05 17:55:53 +0900

    onKeyEvent

--- event/app/src/main/java/com/example/event/MainActivity.kt
+++ event/app/src/main/java/com/example/event/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.event
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.KeyEvent
 import android.view.MotionEvent
 
 class MainActivity : AppCompatActivity() {
@@ -22,5 +23,15 @@ class MainActivity : AppCompatActivity() {
             }
         }
         return super.onTouchEvent(event)
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        Log.d("ggam", "onKeyDown")
+        return super.onKeyDown(keyCode, event)
+    }
+
+    override fun onKeyUp(keyCode: Int, event: KeyEvent?): Boolean {
+        Log.d("ggam", "onKeyUp")
+        return super.onKeyUp(keyCode, event)
     }
 }
